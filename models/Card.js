d3d5b3c793dc8f3ed9745c9478089be8f0b39a53
Robert Schade <roscha80@web.de>
2021-05-30 18:59:55 +0200

    Add requirements to Crad models and User models

--- models/Card.js
+++ models/Card.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const cardSchema = {
-  text: String,
-  author: String,
-  votes: Number,
+  text: { type: String, required: true },
+  author: { type: String, required: true },
+  votes: { type: Number, default: 0 },
 }
 
 module.exports = mongoose.model('Card', cardSchema)
